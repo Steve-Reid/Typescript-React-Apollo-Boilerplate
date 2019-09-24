3e10deb84a658ade1987247025b2f67a67d9b288
Steve Reid <steve@appby.design>
2019-09-24 19:41:30 +0100

    Setup React Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const { data, loading } = useQuery(gql`
+    {
+      hello
+    }
+  `);
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+
+  return <div>{JSON.stringify(data)}</div>;
+};
 
 export default App;
